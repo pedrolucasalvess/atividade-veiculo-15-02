cfa9615523db10088ca556fd5fbd79921a29a43e
Pedro Lucas <pedrolucas.marlier@gmail.com>
2024-10-15 22:02:43 -0300

    atividade

---
+++ Moto.java
@@ -0,0 +1,16 @@
+class Moto implements Veiculo{
+    @Override
+    public void acelerar(){
+        System.out.println("A moto está acelerando.");
+    }
+    
+    @Override
+    public void frear(){
+        System.out.println("A moto está freando.");
+    }
+
+    public void darGrau(){
+        System.out.println("A moto está dando grau.");
+    }
+
+}
